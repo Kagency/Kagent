91357e57c94b5f6c9b19b098acfae6d281102a8e
Kore Nordmann <mail@kore-nordmann.de>
2014-03-07 20:26:47 +0100

    Stubbed annotater

---
+++ src/Kagency/Kagent/Annotater.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Kagency\Kagent;
+
+/**
+ * Class: Annotater
+ *
+ * An annotater annotates the context of a user with additional information.
+ * This can be something trivial like the current time, but also more complex
+ * information like currently running events or something alike.
+ *
+ * @version $Revision$
+ */
+abstract class Annotater
+{
+    /**
+     * Annotate user context with additional information
+     *
+     * @return void
+     */
+    abstract public function annotateContext(Context $context);
+}
