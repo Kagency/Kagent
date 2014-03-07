6540e321f094cf4141d393d57b8031ed2785ee23
Kore Nordmann <mail@kore-nordmann.de>
2014-03-07 20:07:54 +0100

    Added: EventSource base class

---
+++ src/Kagency/Kagent/EventSource.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Kagency\Kagent;
+
+/**
+ * Class: EventSource
+ *
+ * Event sources provide a stream of events. Events can be new tweets or new
+ * entries in a RSS feed or basically anything you can think of. There is now
+ * way to update an event though, so they are create-only.
+ *
+ * @version $Revision$
+ */
+abstract class EventSource
+{
+    /**
+     * Get new events
+     *
+     * Get all new events since the provided revision
+     *
+     * @param string $since
+     * @return Event[]
+     */
+    abstract public function getNewEvents($since);
+}
