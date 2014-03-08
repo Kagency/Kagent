bee9a29281214a16ebf30b4365063cf4d8c82fd4
Kore Nordmann <mail@kore-nordmann.de>
2014-03-08 18:45:04 +0100

    Initialized Behat setup

---
+++ features/bootstrap/FeatureContext.php
@@ -0,0 +1,22 @@
+<?php
+
+use Behat\Behat\Context\BehatContext;
+use Behat\Behat\Exception\PendingException;
+
+/**
+ * Features context.
+ */
+class FeatureContext extends BehatContext
+{
+    /**
+     * Initializes context.
+     *
+     * Initializes context from parameters in behat.yml.
+     *
+     * @param array $parameters
+     */
+    public function __construct(array $parameters)
+    {
+        // Initialize your context here
+    }
+}
