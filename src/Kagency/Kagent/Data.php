1694e42879edd653c092fd67fe8de72156924e73
Kore Nordmann <mail@kore-nordmann.de>
2014-03-09 20:11:33 +0100

    Added: Data struct

---
+++ src/Kagency/Kagent/Data.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Kagency\Kagent;
+
+/**
+ * Class: Data
+ *
+ * Struct representing a data set
+ *
+ * @version $Revision$
+ */
+class Data extends Struct
+{
+    /**
+     * Revision
+     *
+     * @var string
+     */
+    public $revision;
+
+    /**
+     * Data set data
+     *
+     * @var array
+     */
+    public $data;
+}
