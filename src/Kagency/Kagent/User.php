8288952cda609c0990b56e6adfcabc3fb37291cf
Kore Nordmann <mail@kore-nordmann.de>
2014-03-09 19:44:33 +0100

    Defined trivial user object

---
+++ src/Kagency/Kagent/User.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Kagency\Kagent;
+
+/**
+ * Class: User
+ *
+ * Struct representing a user
+ *
+ * @version $Revision$
+ */
+class User extends Struct
+{
+    /**
+     * name
+     *
+     * @var string
+     */
+    public $name;
+}
