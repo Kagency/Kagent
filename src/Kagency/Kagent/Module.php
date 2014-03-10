e46325958e3d7c340918a5b8cb602925c17541b0
Kore Nordmann <mail@kore-nordmann.de>
2014-03-10 09:32:24 +0100

    Stubbed module base class

---
+++ src/Kagency/Kagent/Module.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace Kagency\Kagent;
+
+/**
+ * Class: Module
+ *
+ * This is a base class for modules. It exposes all relevant classes of a
+ * module.
+ *
+ * By default the module does nothing. Methods should be overwritten, as
+ * required, to return the respective implementations the module can provide.
+ *
+ * @TODO: Split up optional module methods into dedicated interfaces? Or just
+ * use DIC tags to discover the respective entities?
+ *
+ * @version $Revision$
+ */
+abstract class Module
+{
+    /**
+     * Initialize module
+     *
+     * @param App $app
+     * @return void
+     */
+    public function initialize(App $app)
+    {
+        // @TODO: Register module services
+        // @TODO: Register module configuration
+        // @TODO: Register module assets
+    }
+
+    /**
+     * Get event sources
+     *
+     * @return EventSource[]
+     */
+    public function getEventSources()
+    {
+        return array();
+    }
+
+    /**
+     * Get data providers
+     *
+     * @return DataProvider[]
+     */
+    public function getDataProviders()
+    {
+        return array();
+    }
+
+    /**
+     * Get annotaters
+     *
+     * @return Annotater[]
+     */
+    public function getAnnotaters()
+    {
+        return array();
+    }
+
+    /**
+     * Get filters
+     *
+     * @return Filter[]
+     */
+    public function getFilters()
+    {
+        return array();
+    }
+
+    /**
+     * Get agents
+     *
+     * @return Agent[]
+     */
+    public function getAgents()
+    {
+        return array();
+    }
+}
